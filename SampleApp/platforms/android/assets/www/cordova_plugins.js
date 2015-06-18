a2534ebd216bf0b9225aa491381b05a590e749c4
Hardik Mansaraa <hardikmansaraa@ymail.com>
2015-06-18 17:36:50 +0530

    add plugins

--- SampleApp/platforms/android/assets/www/cordova_plugins.js
+++ SampleApp/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.apptracker.phonegap.android/www/AppTracker.js",
+        "id": "com.apptracker.phonegap.android.AppTracker",
+        "clobbers": [
+            "AppTracker"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.apptracker.phonegap.android": "0.0.1"
+}
 // BOTTOM OF METADATA
 });
